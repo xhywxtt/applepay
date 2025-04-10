0de044bf5347ed4edff9011c4b44d18c0adab5e2
Haoyue Xue <haoyuexue@microsoft.com>
2025-04-10 20:31:26 +0800

    update absolute return url

--- src/script.js
+++ src/script.js
@@ -68,7 +68,7 @@ expressCheckoutElement.on('confirm', async (event) => {
       // `clientSecret` from the created PaymentIntent
       clientSecret,
       confirmParams: {
-        return_url: './return.html',
+        return_url: 'https://xhywxtt.github.io/applepay/return.html',
       },
     });
 
